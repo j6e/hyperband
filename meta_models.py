6cafcdf8fd4cf3f84593d5fa81a35fade2905317
jgarcia <Joan Garcia>
2017-11-14 11:44:52 +0100

    meta models module

---
+++ meta_models.py
@@ -0,0 +1,59 @@
+import pickle as pickle
+from pprint import pprint
+from functools import partial
+
+from hyperband import Hyperband
+from defs.meta import get_params as get_params_c
+from defs.meta import try_params as try_params_c
+from defs_regression.meta import get_params as get_params_r
+from defs_regression.meta import try_params as try_params_r
+
+
+def classification_meta_model(data, output_file='results.pkl'):
+    if not output_file.endswith('.pkl'):
+        output_file += '.pkl'
+    print("Will save results to", output_file)
+
+    #
+
+    try_params_data = partial(try_params_c, data=data)
+    hb = Hyperband(get_params_c, try_params_data)
+    results = hb.run(skip_last=1)
+
+    print("{} total, best:\n".format(len(results)))
+
+    for r in sorted(results, key=lambda x: x['loss'])[:5]:
+        print("loss: {:.2%} | {} seconds | {:.1f} iterations | run {} ".format(
+            r['loss'], r['seconds'], r['iterations'], r['counter']))
+        pprint(r['params'])
+        print()
+
+    print("saving...")
+
+    with open(output_file, 'wb') as f:
+        pickle.dump(results, f)
+
+
+def regression_meta_model(data, output_file='results.pkl'):
+    if not output_file.endswith('.pkl'):
+        output_file += '.pkl'
+    print("Will save results to", output_file)
+
+    #
+    try_params_data = partial(try_params_r, data=data)
+
+    hb = Hyperband(get_params_r, try_params_data)
+    results = hb.run(skip_last=1)
+
+    print("{} total, best:\n".format(len(results)))
+
+    for r in sorted(results, key=lambda x: x['loss'])[:5]:
+        print("loss: {:.2%} | {} seconds | {:.1f} iterations | run {} ".format(
+            r['loss'], r['seconds'], r['iterations'], r['counter']))
+        pprint(r['params'])
+        print()
+
+    print("saving...")
+
+    with open(output_file, 'wb') as f:
+        pickle.dump(results, f)
---
+++ meta_test.py
@@ -0,0 +1,4 @@
+from load_data_for_regression import data
+from meta_models import regression_meta_model
+
+regression_meta_model(data, 'try')
